f8902619f490dd5df868c490a5553f697c3f87af
Igor <126359649+GalaxyIgor@users.noreply.github.com>
2025-09-28 15:57:47 -0300

    refactor: Melhora shell.sh e corrige execução no pipeline

--- CRM/scripts/shell.sh
+++ CRM/scripts/shell.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 cd CRM
 ls
 echo "Pipeline executado!"
 pwd
-sudo apt-get install mailutils
-echo "Apenas testando " | mail -s "a subject" "$EMAIL_RECIPIENT"
+sudo apt-get update && sudo apt-get install -y mailutils
+echo "Apenas testando " | mail -s "Pipeline CI/CD executado" "$EMAIL_RECIPIENT"
